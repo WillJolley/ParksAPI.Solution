e7c3b26528c163ab8ba641a5fb400efc838bd6e8
WillJolley <yeswilljolley@gmail.com>
2023-11-24 21:18:02 -0800

    add Park model

---
+++ ParksAPI/Models/Park.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkAPI.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Features { get; set; }
+  }
+}
